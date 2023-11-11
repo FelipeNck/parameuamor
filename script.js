5d6dd0db77915da1a5d1ac5b16d8dc3fbe0a4819
FelipeNck <felipenocko17@gmail.com>
2023-11-11 13:15:21 -0300

    alteracoes

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let no = document.querySelector("#no");
-let si = document.querySelector("#si");
+let positions = [0,0];
 
-no.addEventListener("click", () =>{
-    let vert = Math.floor(Math.random() * 300);
+function move() {
+    let vert = Math.floor(Math.random() * (300 - 100 + 1)) + 100;
     if (Math.floor(Math.random() * 1) == 1){vert = vert*-1;}
-    let hori = Math.floor(Math.random() * 100);
+    let hori = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
     if (Math.floor(Math.random() * 1) == 1){hori = hori*-1;}
     no.style.top = vert+"px";
     no.style.left = hori+"px";
-});
+    positions[0] = vert;
+    positions[1] = hori;
+    console.log(no)
+}
 
-si.addEventListener("click", () => {
-    let certo = document.querySelectorAll("p");
-    console.log(certo);
-    for (let i=0; i < certo.length; i++){
-        certo[i].style.display = "block";
+function remove() {
+    no.onclick = "";
+    let correct = document.querySelectorAll("p");
+    for (let i=0; i < correct.length; i++){
+        correct[i].style.display = "block";
     }
-})
+}
+
